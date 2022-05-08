653960d471643ffe36f43a6d3738c838e50dc149
vinualex21 <alexvinu2001@gmail.com>
2022-05-08 17:41:27 +0100

    Exercise002 implemented. New unit test conditions added.

---
+++ Exercises/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercises
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, string location, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Location = location;
+            Age = age;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+        public int Age { get; set; }
+    }
+}
