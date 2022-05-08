1821de02acd32278eb3cfc0452680b1b52aebdf7
vinualex21 <alexvinu2001@gmail.com>
2022-05-08 16:34:30 +0100

    Exercise001 implemented. Additional test cases added.

--- Exercises.Tests/Exercise001Tests.cs
+++ Exercises.Tests/Exercise001Tests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Exercises.Models;
 using System.Collections.Generic;
+using System;
 
 namespace Exercises.Tests
 {
@@ -21,6 +22,9 @@ namespace Exercises.Tests
             Exercise001.CapitalizeWord("hello").Should().Be("Hello");
             Exercise001.CapitalizeWord("oh no, bears!!").Should().Be("Oh no, bears!!");
             Exercise001.CapitalizeWord("the quick fox").Should().Be("The quick fox");
+            Exercise001.CapitalizeWord(null).Should().Be(null);
+            Exercise001.CapitalizeWord(" ").Should().Be(" ");
+            Exercise001.CapitalizeWord(string.Empty).Should().Be(string.Empty);
         }
 
         [Test]
@@ -28,6 +32,9 @@ namespace Exercises.Tests
         {
             Exercise001.GenerateInitials("Frederic", "Bonneville").Should().Be("F.B");
             Exercise001.GenerateInitials("James", "Bond").Should().Be("J.B");
+            Exercise001.GenerateInitials("Mathew", null).Should().Be("M");
+            Exercise001.GenerateInitials("", "Wick").Should().Be("W");
+            Exercise001.GenerateInitials(null, null).Should().Be("");
         }
 
         [Test]
@@ -37,13 +44,34 @@ namespace Exercises.Tests
             Exercise001.AddVat(40, 17.5).Should().Be(47);
             Exercise001.AddVat(33.5, 17.5).Should().Be(39.36);
             Exercise001.AddVat(25, 0).Should().Be(25);
+
+            var ex = Assert.Throws<ArgumentException>(() => Exercise001.AddVat(-25, 0));
+            Assert.That(ex.Message, Is.EqualTo("Price cannot be negative. Please enter a valid price."));
+            ex = Assert.Throws<ArgumentException>(() => Exercise001.AddVat(25, -5));
+            Assert.That(ex.Message, Is.EqualTo("VAT cannot be negative. Please enter a valid VAT."));
         }
+
+        [Test]
+        public void AddVat_Should_Throw_Argument_Exception()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Exercise001.AddVat(-25, 0));
+            Assert.That(ex.Message, Is.EqualTo("Price cannot be negative. Please enter a valid price."));
+            ex = Assert.Throws<ArgumentException>(() => Exercise001.AddVat(25, -5));
+            Assert.That(ex.Message, Is.EqualTo("VAT cannot be negative. Please enter a valid VAT."));
+        }
+
+
 
         [Test]
         public void Reverse_Should_Return_Correctly_Reversed_String()
         {
             Exercise001.Reverse("foo").Should().Be("oof");
             Exercise001.Reverse("why would you even want to do this?").Should().Be("?siht od ot tnaw neve uoy dluow yhw");
+            Exercise001.Reverse("x").Should().Be("x");
+            Exercise001.Reverse(null).Should().Be(null);
+            Exercise001.Reverse("level").Should().Be("level");
+            Exercise001.Reverse("why not numb3r5").Should().Be("5r3bmun ton yhw");
+
         }
 
         [Test]
@@ -82,6 +110,15 @@ namespace Exercises.Tests
             };
 
             Exercise001.CountLinuxUsers(users).Should().Be(2);
+        }
+
+        [Test]
+        public void CountLinuxUsers_Should_Return_Zero_For_Null_Or_Empty_Input()
+        {
+            // This is using the latest C# language features
+            // This is the same as doing User u1 = new User("Heather", "Windows 10", "Windows");
+            Exercise001.CountLinuxUsers(null).Should().Be(0);
+            Exercise001.CountLinuxUsers(new List<User>()).Should().Be(0);
         }
     }
 }
--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Exercises.Models;
 
 namespace Exercises
@@ -8,32 +10,52 @@ namespace Exercises
     {
         public string CapitalizeWord(string word)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(word))
+                return word;
+            else
+                return char.ToUpper(word[0]) + word.Substring(1);
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            StringBuilder intials = new StringBuilder();
+
+            if (!string.IsNullOrEmpty(firstName?.Trim()))
+                intials.Append(firstName[0]);
+            if (!string.IsNullOrEmpty(lastName?.Trim()))
+            {
+                if (intials.Length > 0)
+                    intials.Append(".");
+                intials.Append(lastName[0]);
+            }
+            return intials.ToString();
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if (originalPrice < 0)
+                throw new ArgumentException("Price cannot be negative. Please enter a valid price.");
+            if (vatRate < 0)
+                throw new ArgumentException("VAT cannot be negative. Please enter a valid VAT.");
+            return Math.Round((originalPrice * vatRate / 100) + originalPrice, 2);
+
         }
 
         public string Reverse(string sentence)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if (sentence == null || sentence.Length <= 1)
+                return sentence;
+            else
+            {
+                var charArray = sentence.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
         }
 
         public int CountLinuxUsers(List<User> users)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return (users == null || !users.Any()) ? 0 : users.Count(u => u.Type == "Linux");
         }
     }
 }
