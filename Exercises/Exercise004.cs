1350823bb0a44a88be731c4a7e8c3c8d7f01e7b1
vinualex21 <alexvinu2001@gmail.com>
2022-05-08 21:54:34 +0100

    Exercise004 implemented.

--- Exercises/Exercise004.cs
+++ Exercises/Exercise004.cs
@@ -5,8 +5,7 @@ namespace Exercises
     {
         public static DateTime AddGigaSecond(DateTime dateTime)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return dateTime.AddSeconds(1000000000);
         }
     }
 }
