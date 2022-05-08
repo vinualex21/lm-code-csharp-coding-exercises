8d10f3e392a672e48921534324c9617181f582c9
vinualex21 <alexvinu2001@gmail.com>
2022-05-08 19:39:20 +0100

    Exercise003 implemented.

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -3,10 +3,11 @@ namespace Exercises
 {
     public class Exercise003
     {
-        // Replace the exception statement below with your code!
-        public string[] IceCreamFlavours => throw new NotImplementedException();
+        public string[] IceCreamFlavours = new string[] { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
 
-        // Replace the exception statement below with your code!
-        public int IceCreamCode(string iceCreamFlavour) => throw new NotImplementedException();
+        public int IceCreamCode(string iceCreamFlavour)
+        {
+            return Array.IndexOf(IceCreamFlavours, iceCreamFlavour);
+        }
     }
 }
